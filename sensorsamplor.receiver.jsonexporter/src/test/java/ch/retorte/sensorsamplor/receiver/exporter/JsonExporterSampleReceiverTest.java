a3b67ed3ccc94ff287e1e473fdbb009de3d07aff
Nico Waldispühl <nw@retorte.ch>
2015-04-26 12:01:38 +0200

    Fixed broken test.

--- sensorsamplor.receiver.jsonexporter/src/test/java/ch/retorte/sensorsamplor/receiver/exporter/JsonExporterSampleReceiverTest.java
+++ sensorsamplor.receiver.jsonexporter/src/test/java/ch/retorte/sensorsamplor/receiver/exporter/JsonExporterSampleReceiverTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class JsonExporterSampleReceiverTest {
 
-  JsonExporterSampleReceiver sut = new JsonExporterSampleReceiver("", 1);
+  JsonExporterSampleReceiver sut = new JsonExporterSampleReceiver("", 1, "", "");
 
 
   @Test
