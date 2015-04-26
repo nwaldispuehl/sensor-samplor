703b6465347c7f572f886b94e694448dba635a6e
Nico Waldispühl <nw@retorte.ch>
2015-04-26 16:29:20 +0200

    Fixed tests which failed due to timezone issues.

--- sensorsamplor.receiver.graphite/src/test/java/ch/retorte/sensorsamplor/receiver/graphite/GraphiteSampleReceiverTest.java
+++ sensorsamplor.receiver.graphite/src/test/java/ch/retorte/sensorsamplor/receiver/graphite/GraphiteSampleReceiverTest.java
@@ -2,11 +2,12 @@ package ch.retorte.sensorsamplor.receiver.graphite;
 
 import ch.retorte.sensorsamplor.sensor.Sample;
 import ch.retorte.sensorsamplor.sensor.TransferSample;
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.TimeZone;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -17,14 +18,12 @@ import static org.mockito.Mockito.spy;
  */
 public class GraphiteSampleReceiverTest {
 
-  private DateTimeFormatter formatter = DateTimeFormat.forPattern("YYYY-MM-dd");
-
   private GraphiteSampleReceiver sut = new GraphiteSampleReceiver("", 0, "", "");
   private Sample sample;
 
   @Before
   public void setup() {
-    sample = sampleOf("2015-04-25", "myPlatform", "mySensor");
+    sample = sampleOf(2015, 04, 25, "myPlatform", "mySensor");
   }
 
   @Test
@@ -41,21 +40,21 @@ public class GraphiteSampleReceiverTest {
 
   @Test
   public void shouldProduceTimestampOfSample() {
-    assertProduces(sut.timestampOf(sample), "1429912800");
+    assertProduces(sut.timestampOf(sample), "1429920000");
   }
 
   @Test
   public void shouldProduceGraphiteFormatOfSample() {
-    assertProduces(sut.graphiteFormatOf(sample, "myKey1"), "myPlatform.mySensor.myKey1 123 1429912800");
-    assertProduces(sut.graphiteFormatOf(sample, "myKey2"), "myPlatform.mySensor.myKey2 abc 1429912800");
+    assertProduces(sut.graphiteFormatOf(sample, "myKey1"), "myPlatform.mySensor.myKey1 123 1429920000");
+    assertProduces(sut.graphiteFormatOf(sample, "myKey2"), "myPlatform.mySensor.myKey2 abc 1429920000");
   }
 
   private void assertProduces(String input, String expectedOutput) {
     assertThat(input, is(expectedOutput));
   }
 
-  private Sample sampleOf(String timestamp, String platformIdentifier, String sensorType) {
-    TransferSample sample = spy(new TransferSample(formatter.parseDateTime(timestamp).toDateTime(DateTimeZone.UTC), platformIdentifier, sensorType));
+  private Sample sampleOf(int year, int month, int day, String platformIdentifier, String sensorType) {
+    TransferSample sample = spy(new TransferSample(new DateTime(year, month, day, 0, 0, DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT"))), platformIdentifier, sensorType));
     sample.addItem("myKey1", 123);
     sample.addItem("myKey2", "abc");
     return sample;
